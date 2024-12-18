05b2d9f424307f7ddce39583ef555e72a723cff9
Dallon DeLoach <dallon.deloach@pfgc.com>
2024-12-18 09:19:32 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 import express from 'express';
-import readGeminiRoute from './src/api/readGeminiApi.js'
-import writeGeminiRoute from './src/api/writeGeminiApi.js'
-import readSqlRoute from './src/api/readSqlApi.js'
-import writeSqlRoute from './src/api/writeSqlApi.js'
-
+import path from 'path';
+import readGeminiRoute from './src/api/readGeminiApi.js';
+import writeGeminiRoute from './src/api/writeGeminiApi.js';
+import readSqlRoute from './src/api/readSqlApi.js';
+import writeSqlRoute from './src/api/writeSqlApi.js';
 
 const app = express();
 const PORT = 3001;
 
-// Middleware to parse JSON request bodies
-app.use(express.json());
+// Serve static files
+app.use('/sandbox/dist', express.static(path.resolve('./dist')));
 
-// Use the routes
-app.use(readGeminiRoute);
-app.use(writeGeminiRoute);
-app.use(writeSqlRoute);
-app.use(readSqlRoute);
+// Proxy API routes
+app.use('/sandbox/src/api/readGemini', readGeminiRoute);
+app.use('/sandbox/src/api/writeGemini', writeGeminiRoute);
+app.use('/sandbox/src/api/readSql', readSqlRoute);
+app.use('/sandbox/src/api/writeSql', writeSqlRoute);
+
+// Serve index.html for any other route
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('./dist/index.html'));
+});
 
 // Start the server
 app.listen(PORT, () => {
